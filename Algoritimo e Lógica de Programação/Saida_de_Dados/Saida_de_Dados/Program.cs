c618ca6cc5fbeb6119bff61df1b9674af3e78560
Leonardo Brito <92063418+Leoguibrito@users.noreply.github.com>
2022-10-30 22:39:43 -0300

    Add files via upload

---
+++ Algoritimo e Lógica de Programação/Saida_de_Dados/Saida_de_Dados/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Saida_de_Dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            double x;
+            x = 2.3241;
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(x.ToString(CI));    
+        }
+    }
+}
---
+++ Algoritimo e Lógica de Programação/Vetores/Vetores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int N;
+            Console.Write("Digite quantos numeros serao digitados: ");
+            N = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[N];
+           
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Digite o numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(vet[i].ToString("F1", CI));
+            }
+        }
+    }
+}
