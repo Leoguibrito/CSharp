b1ae0c17781c52446dc32730f6fb3d38bc661fc8
Leonardo Brito <92063418+Leoguibrito@users.noreply.github.com>
2022-10-31 23:25:59 -0300

    Add files via upload

---
+++ C# Completo OOP + Projetos/MaiorTriangulo/MaiorTriangulo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaiorTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do Triangulo X: ");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com as medidas do Triangulo Y: ");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double areaX = 0;
+            double areaY = 0;
+
+            if (x.Verificar())
+            {
+                areaX = x.Area();
+            }
+
+            if (y.Verificar())
+            {
+                areaY = y.Area();
+            }
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("A maior area é a do triangulo X");
+            } else
+            {
+                Console.WriteLine("A maior area é a do triangulo Y");
+            }
+        }
+    }
+}
---
+++ C# Completo OOP + Projetos/MaiorTriangulo/MaiorTriangulo/Triangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaiorTriangulo
+{
+    internal class Triangulo
+    {
+        public double A, B, C;
+
+        public bool Verificar()
+        {
+            bool triangulo_ = false;
+
+            if (A < B + C && B < A + C && C < B + A)
+            {
+                triangulo_ = true;
+            }
+
+            return triangulo_;
+        }
+        public double Area ()
+        {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+        }
+    }
+}
