9a8277055cdb0fd259f6e6c3bbb9ac2be593d51d
Leonardo Brito <92063418+Leoguibrito@users.noreply.github.com>
2022-10-31 23:26:45 -0300

    Add files via upload

---
+++ C# Completo OOP + Projetos/ProdutoM/Course/Produto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutoM
+{
+    internal class Produto
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public string Nome;
+        public double Preco;
+        public int Estoque;
+
+        public Produto() {}
+
+        public Produto(string nome, double preco, int estoque)
+        {
+            Nome = nome;
+            Preco = preco;
+            Estoque = estoque;
+        }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Estoque = 10;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Estoque;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Estoque += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Estoque -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $" + Preco.ToString("F2", CI) + ", " + Estoque 
+            + " unidades, Total: $" + ValorTotalEmEstoque().ToString("F2", CI);
+
+        }
+    }
+}
---
+++ C# Completo OOP + Projetos/ProdutoM/Course/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutoM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+
+
+            Console.WriteLine("Entre os dados do produto");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Quantidade no estoque: ");
+            int estoque = int.Parse(Console.ReadLine());
+            Console.Write("Preço do produto: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, estoque);
+            Produto q = new Produto(nome, preco);
+            Produto w = new Produto
+            {
+                Nome = nome,
+                Preco = preco,
+                Estoque = estoque
+            };
+
+            Console.WriteLine("Dados do Produto: " + p);
+            Console.WriteLine("Dados do Produto: " + q);
+            Console.WriteLine();
+
+            int qtde;
+            Console.Write("Digite quantos produtos serão adicionados: ");
+            qtde = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qtde);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do Produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite quantos produtos serão removidos: ");
+            qtde = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtde);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do Produto: " + p);
+        }
+    }
+}
