ad32b4500819d2d8bbad249a50442f5ffdf54a89
Leonardo Brito <92063418+Leoguibrito@users.noreply.github.com>
2022-10-31 23:30:10 -0300

    Add files via upload

---
+++ C# Completo OOP + Projetos/Esfera/Esfera/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2", CI));
+            Console.WriteLine("Circunferencia: " + Calculadora.Circunferencia(raio).ToString("F2", CI));
+            Console.WriteLine("Volume: " + Calculadora.Volume(raio).ToString("F2", CI));
+        }
+    }
+}
